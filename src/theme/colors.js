31af16ceafaf7e16e69340fc501d57e625a770c1
HaiBui <hai.bui@fruitful.io>
2019-09-26 11:15:00 +0700

    add themes

---
+++ src/theme/colors.js
@@ -0,0 +1,31 @@
+/**
+ * App Theme - Colors
+ */
+const app = {
+  white: '#FFF',
+  
+};
+
+
+
+const text = {
+  
+};
+
+const borders = {
+  border: '#DFDFDF',
+};
+
+
+const dialog = {
+  dialogBody: 'rgba(238, 241, 242, 1)',
+  dialogDivider: 'rgba(205, 217, 223, 1)',
+}
+
+export default {
+  ...app,
+  ...text,
+  ...borders,
+  ...dialog,
+
+};
---
+++ src/theme/styles.js
@@ -0,0 +1,17 @@
+/**
+ * App Styles
+ */
+import Colors from './colors';
+import Fonts from './fonts';
+import Sizes from './sizes';
+import { Platform } from 'react-native';
+import { AppColors } from './index';
+
+
+export default {
+  // Import app styles base
+  appContainer: {
+    backgroundColor: Colors.white,
+    height: '100%',
+  },
+}
