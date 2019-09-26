23d81a5267b4ce75fef3bb6dbf22d52615cc7fec
hatructu <hatructu@gmail.com>
2019-09-26 10:42:46 +0700

    tabs

--- src/component/TabManager.js
+++ src/component/TabManager.js
@@ -9,7 +9,9 @@ import {
 import { Router, Scene, Stack, Tabs } from 'react-native-router-flux'
 import {Icon} from 'react-native-elements'
 import ListUser from './listUser/ListUser'
-
+import ListExercise from './listExercise/ListExercise'
+import Calendar from './calendar/Calendar'
+import Report from './report/Report'
 const TabsIcon = ({...props }) => {
     const { focused, iconActive, iniconActive } = props
     return (
@@ -32,11 +34,44 @@ const tabScreen = () => {
                         hideNavBar={true}
                         icon={TabsIcon}
                         iconActive={require('@icons/users_check.png')}
-                        iniconActive={require('@icons/users_check.png')}
+                        iniconActive={require('@icons/users.png')}
                     >
                         <Scene
                             component={ListUser}
                             initial
+                        />
+                    </Stack>
+
+                    <Stack
+                        hideNavBar={true}
+                        icon={TabsIcon}
+                        iconActive={require('@icons/finess_check.png')}
+                        iniconActive={require('@icons/finess.png')}
+                    >
+                        <Scene
+                            component={ListExercise}
+                        />
+                    </Stack>
+
+                    <Stack
+                        hideNavBar={true}
+                        icon={TabsIcon}
+                        iconActive={require('@icons/event_check.png')}
+                        iniconActive={require('@icons/event.png')}
+                    >
+                        <Scene
+                            component={Calendar}
+                        />
+                    </Stack>
+
+                    <Stack
+                        hideNavBar={true}
+                        icon={TabsIcon}
+                        iconActive={require('@icons/investments.png')}
+                        iniconActive={require('@icons/invest.png')}
+                    >
+                        <Scene
+                            component={Report}
                         />
                     </Stack>
                 </Tabs>
