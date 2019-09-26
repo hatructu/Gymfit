788a5bab5d084cd1927ae008beb83a87743d0c5c
hatructu <hatructu@gmail.com>
2019-09-26 10:43:37 +0700

    no message

---
+++ src/component/calendar/Calendar.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+} from 'react-native'
+import { Icon } from 'react-native-elements'
+
+export default class Calendar extends Component {
+    render() {
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
---
+++ src/component/report/Report.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import {
+    View,
+    Text,
+    StyleSheet,
+    TouchableOpacity,
+    Image,
+} from 'react-native'
+import { Icon } from 'react-native-elements'
+
+export default class Report extends Component {
+    render() {
+        return (
+            <View>
+                
+            </View>
+        )
+    }
+}
