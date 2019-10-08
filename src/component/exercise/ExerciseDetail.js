753111488e9a51457d38e8dc8eb7d2c022d3e5d5
ThangNguyen <>
2019-10-08 10:45:03 +0700

    update detail

--- src/component/exercise/ExerciseDetail.js
+++ src/component/exercise/ExerciseDetail.js
@@ -1,5 +1,13 @@
 import React, {Component} from 'react';
-import {View, Text, SafeAreaView, Image, TextInput, Button} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  Button,
+  ScrollView,
+} from 'react-native';
 import {AppColors, AppFonts, AppStyles, AppSizes} from '@theme';
 
 import {Actions} from 'react-native-router-flux';
@@ -15,36 +23,40 @@ export default class UserDetail extends Component {
       height: 0,
       bodymath: 0,
       phoneNumber: 0,
-  
     };
-    
-    this.item = props.dataExercise
+
+    this.item = props.dataExercise;
   }
   render() {
-    let {name, round, point, image} = this.item
+    let {name, round, point, image} = this.item;
     return (
       <View
         style={{
           flex: 1,
           alignItems: 'center',
-          justifyContent:'center',
+          justifyContent: 'center',
 
           backgroundColor: AppColors.backgroundRegister,
         }}>
-        <SafeAreaView style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent:'center',
+        <SafeAreaView
+          style={{
+            flex: 1,
+            alignItems: 'center',
+            justifyContent: 'center',
 
-          backgroundColor: AppColors.backgroundRegister,
-        }}>
-          
-          <Text style={[AppStyles.whiteText,{}]}> THÔNG TIN BÀI TẬP</Text>
-          <Image style={{width: 200, height: 160, margin: 10}} source={{uri:image}} />
-          <Text style={AppStyles.textView}>Tên bài tập: {name}</Text>
-          <Text style={AppStyles.textView}>Số hiệp : {round}</Text>
-          <Text style={AppStyles.textView}>Điểm số : {point}</Text>
-          <Button title='Quay lại' onPress={()=>Actions.ListExercise()} />
+            backgroundColor: AppColors.backgroundRegister,
+          }}>
+          <ScrollView>
+            <Text style={[AppStyles.whiteText, {}]}> THÔNG TIN BÀI TẬP</Text>
+            <Image
+              style={{width: 200, height: 160, margin: 10}}
+              source={{uri: image}}
+            />
+            <Text style={AppStyles.textView}>Tên bài tập: {name}</Text>
+            <Text style={AppStyles.textView}>Số hiệp : {round}</Text>
+            <Text style={AppStyles.textView}>Điểm số : {point}</Text>
+            <Button title="Quay lại" onPress={() => Actions.ListExercise()} />
+          </ScrollView>
         </SafeAreaView>
       </View>
     );
--- src/component/user/UserDetail.js
+++ src/component/user/UserDetail.js
@@ -1,5 +1,13 @@
 import React, {Component} from 'react';
-import {View, Text, SafeAreaView, Image, TextInput} from 'react-native';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+  StyleSheet
+} from 'react-native';
 import {AppColors, AppFonts, AppStyles, AppSizes} from '@theme';
 import {Input, Button} from 'react-native-elements';
 import {Actions} from 'react-native-router-flux';
@@ -16,38 +24,44 @@ export default class UserDetail extends Component {
       bodymath: 0,
       phoneNumber: 0,
     };
-    
-    this.item = props.dataMember
+
+    this.item = props.dataMember;
   }
   render() {
-    let {name, sex, weight, height, bodymath, phoneNumber, avatar} = this.item
+    let {name, sex, weight, height, bodymath, phoneNumber, avatar} = this.item;
     return (
-      <View
-        style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent:'center',
-
-          backgroundColor: AppColors.backgroundRegister,
-        }}>
-        <SafeAreaView style={{
-          flex: 1,
-          alignItems: 'center',
-          justifyContent:'center',
-
-          backgroundColor: AppColors.backgroundRegister,
-        }}>
+      <View style={styles.container}>
+        <SafeAreaView style={styles.container}>
           
-          <Text style={[AppStyles.whiteText,{}]}> THÔNG TIN HỌC VIÊN</Text>
-          <Image style={{width: 200, height: 160, margin: 10}} source={{uri:avatar}} />
-          <Text style={AppStyles.textView}>Tên : {name}</Text>
-          <Text style={AppStyles.textView}>Giới tính : {sex == true ? "nam" : "nữ"}</Text>
-          <Text style={AppStyles.textView}>Cân nặng : {weight}</Text>
-          <Text style={AppStyles.textView}>Chiều cao : {height} </Text>
-          <Text style={AppStyles.textView}>Chỉ số cơ thể : {bodymath}</Text>
-          <Text style={AppStyles.textView}>Số điện thoại : {phoneNumber}</Text>
+            <Text style={[AppStyles.whiteText, {}]}> THÔNG TIN HỌC VIÊN</Text>
+            <Image
+              style={{width: 200, height: 160, margin: 10}}
+              source={{uri: avatar}}
+            />
+            <Text style={AppStyles.textView}>Tên : {name}</Text>
+            <Text style={AppStyles.textView}>
+              Giới tính : {sex == true ? 'nam' : 'nữ'}
+            </Text>
+            <Text style={AppStyles.textView}>Cân nặng : {weight}</Text>
+            <Text style={AppStyles.textView}>Chiều cao : {height} </Text>
+            <Text style={AppStyles.textView}>Chỉ số cơ thể : {bodymath}</Text>
+            <Text style={AppStyles.textView}>
+              Số điện thoại : {phoneNumber}
+            </Text>
+          
         </SafeAreaView>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+  flex: 1,
+          alignItems: 'center',
+          justifyContent: 'center',
+
+          backgroundColor: AppColors.backgroundRegister,
+  }
+});
+
